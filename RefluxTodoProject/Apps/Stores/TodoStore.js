f48940b0c7659e880a8be209a4109f98497b82ff
Akshat Paul <akshatpaul@gmail.com>
2015-11-15 01:05:11 +0530

    adding reflux example

---
+++ RefluxTodoProject/Apps/Stores/TodoStore.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var Reflux = require('reflux');
+var _ = require("underscore");
+
+module.exports = Reflux.createStore({
+  listenables: [require('../Actions/TodoActions')],
+
+  todos: {},
+
+  onTodoCreate: function(text) {
+    var id = (new Date()).toString(36);
+    console.log(id);
+    this.todos[id] = {
+      id: id,
+      complete: false,
+      text: text,
+    };
+    this.trigger(null);
+  },
+  onTodoUpdate: function(id, updates) {
+    this.todos[id] = _.extend({}, this.todos[id], updates);
+    this.trigger(null);
+  },
+  onTodoComplete: function(id) {
+    this.onTodoUpdate(id, {complete: true});
+  },
+  onTodoUndoComplete: function(id) {
+    this.onTodoUpdate(id, {complete: false});
+  },
+  onTodoDestroy: function(id) {
+    delete this.todos[id];
+    this.trigger(null);
+  },
+
+  getAll: function() {
+    return _.values(this.todos);
+  },
+});
